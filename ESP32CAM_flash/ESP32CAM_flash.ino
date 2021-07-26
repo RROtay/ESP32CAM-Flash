b643eb9fa77337ce31770796282955a6f10b5621
Ramírez O <66526781+RROtay@users.noreply.github.com>
2021-07-26 15:53:37 -0500

    Update ESP32CAM_flash.ino
    Fork hecho para cuestioens didácticas

--- ESP32CAM_flash/ESP32CAM_flash.ino
+++ ESP32CAM_flash/ESP32CAM_flash.ino
@@ -2,14 +2,17 @@
 //Por: Hugo Vargas
 //24 de mayo de 2021
 //Creditos Moayom
+//modificado por : RROtay
+//  >> por motivos didácticos del uso de GitHub-desktop    
 //
 //Este es el programa básico para controlar el
 //LED Flash del ESP32CAM. Este programa parte del
 //hecho de que cuentas con la base programadora.
 
 //Definición de pines
-#define LAMP 4 //Se selecciona 
-
+//  >> Línea original
+//#define LAMP 4 //Se selecciona 
+#define LAMP 5 // nueva línea, solo borrar línea y descomentar la anterior
 //Inicialización del programa
 void setup() //Estructura principal
 {
